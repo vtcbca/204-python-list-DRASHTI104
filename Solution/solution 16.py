c8e6415a3a5088b6a347e93d5c733a766a6213f6
Drashti <100269256+DRASHTI104@users.noreply.github.com>
2022-03-31 12:38:36 +0530

    solution 16

---
+++ Solution/solution 16.py
@@ -0,0 +1,7 @@
+#name:-Drashti104
+string=input("Enter any string:")
+if (string==string[::-1]):
+    print("{} is pelindrom word:".format(string))
+else:
+    print("{} is not pelindrom word:".format(string))
+
